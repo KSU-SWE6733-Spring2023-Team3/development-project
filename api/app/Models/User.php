1edc5908789f440ebd87872c61e71da509a3e705
Elliott Allmann <elliott.allmann@cox.net>
2023-03-08 20:16:51 -0800

    Updated user model to hook into NeoEloquent ORM

--- api/app/Models/User.php
+++ api/app/Models/User.php
@@ -8,9 +8,13 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 
-class User extends Authenticatable
+use NeoEloquent;
+
+class User extends NeoEloquent
 {
     use HasApiTokens, HasFactory, Notifiable;
+
+    protected $label = 'User';
 
     /**
      * The attributes that are mass assignable.
