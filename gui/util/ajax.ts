8ebb3cc3c3db441b0549266bb5ec66ee79032d5e
Elliott Allmann <elliott.allmann@cox.net>
2023-04-19 19:43:17 -0700

    Updates from testing, should avoid 401 unauthorized.

--- gui/util/ajax.ts
+++ gui/util/ajax.ts
@@ -2,8 +2,8 @@
 import axios from 'axios';
 axios.defaults.withCredentials = true;
 
-const BASE_URL = "http://localhost:8000/";
-//const BASE_URL = "http://35.202.95.163:8080/";
+//const BASE_URL = "http://localhost:8000/";
+const BASE_URL = "http://35.202.95.163:8080/";
 
 const postRequest = async function post(url: string, data) {
     await axios.get(BASE_URL + 'sanctum/csrf-cookie', {
