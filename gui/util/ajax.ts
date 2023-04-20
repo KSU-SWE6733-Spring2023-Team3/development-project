dec2c2418d6654212990f099279fbce96e37305e
Elliott Allmann <elliott.allmann@cox.net>
2023-04-19 19:18:00 -0700

    await the initial promise for csrf cookie

--- gui/util/ajax.ts
+++ gui/util/ajax.ts
@@ -1,26 +1,27 @@
 
 import axios from 'axios';
+axios.defaults.withCredentials = true;
 
 const BASE_URL = "http://localhost:8000/";
 //const BASE_URL = "http://35.202.95.163:8080/";
 
 const postRequest = async function post(url: string, data) {
-    return await axios.get(BASE_URL + 'sanctum/csrf-cookie', {
+    await axios.get(BASE_URL + 'sanctum/csrf-cookie', {
         withCredentials: true
-    }).then(response => {
-        return axios.post(BASE_URL + url, data, {
+    });
+
+    return axios.post(BASE_URL + url, data, {
            withCredentials: true
-        });
     });
 };
 
 const getRequest = async function get(url: string) {
-    return await axios.get(BASE_URL + 'sanctum/csrf-cookie', {
+    await axios.get(BASE_URL + 'sanctum/csrf-cookie', {
         withCredentials: true
-    }).then(response => {
-       return axios.get(BASE_URL + url, {
+    });
+
+    return axios.get(BASE_URL + url, {
            withCredentials: true
-       });
     });
 };
 
